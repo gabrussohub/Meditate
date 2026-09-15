990b6c315f3c97cb15406fbedea09e4e49678110
gabrussohub <gabriel.frusso@gmail.com>
2026-09-15 16:00:00 -0300

    Aplica paleta Headspace (creme claro, azul acao, acentos)

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 /* Cache do app (shell). Áudios grandes ficam em cache sob demanda. */
-const CACHE = "sonia-med-v3";
+const CACHE = "sonia-med-v4";
 const SHELL = ["./", "index.html", "styles.css", "app.js", "meditations.json", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", (e) => {
